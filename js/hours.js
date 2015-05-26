aa9ec5f1148c18683754bb9f9a5e6b973254a6a2
Josh Bivens <hotbioform@gmail.com>
2015-05-26 16:31:49 -0600

    Change indents

--- js/hours.js
+++ js/hours.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
   var day = date.getDay();
   var hour = date.getHours();
   if(day > 0 && day < 5 && hour >= 9 && hour < 19){
-		$('#open-close').html('<p>Open</p>').css('color', '#4be74b');
+    $('#open-close').html('<p>Open</p>').css('color', '#4be74b');
   }else if(day > 5 && day < 7 && hour >= 9 && hour < 21){
-		$('#open-close').html('<p>Open</p>').css('color', '#4be74b');
+    $('#open-close').html('<p>Open</p>').css('color', '#4be74b');
   }else{
     $('#open-close').html('<p>Closed</p>').css('color', '#fb260e');
   }
