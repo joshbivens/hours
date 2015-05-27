2071da09ed94612b3993128e7af4a7ff41112dba
Josh Bivens <hotbioform@gmail.com>
2015-05-27 09:25:25 -0600

    Refactor to use ternary ops

--- js/hours.js
+++ js/hours.js
@@ -1,12 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
   var date = new Date();
   var day = date.getDay();
   var hour = date.getHours();
-  if(day > 0 && day < 5 && hour >= 9 && hour < 19){
-    $('#open-close').html('<p>Open</p>').css('color', '#4be74b');
-  }else if(day > 5 && day < 7 && hour >= 9 && hour < 21){
-    $('#open-close').html('<p>Open</p>').css('color', '#4be74b');
+
+  var openMT = hour >= 9 && hour < 19 && day > 0 && day < 5 ? true : false;
+  var openFS = hour >= 9 && hour < 21 && day > 3 && day < 6  ? true : false;
+
+  if(openMT){
+    $('#open-close').html('<b>Open</b>').css('color', '#4be74b');
+  }else if(openFS){
+    $('#open-close').html('<b>Open</b>').css('color', '#4be74b');
   }else{
-    $('#open-close').html('<p>Closed</p>').css('color', '#fb260e');
+    $('#open-close').html('<b>Closed</b>').css('color', '#fb260e');
   }
+
 });
