ea0818c7772bb546673ef62c942b8f2274ff06e1
Josh Bivens <hotbioform@gmail.com>
2015-05-27 09:26:43 -0600

    Fix whitespace

--- js/hours.js
+++ js/hours.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   var hour = date.getHours();
 
   var openMT = hour >= 9 && hour < 19 && day > 0 && day < 5 ? true : false;
-  var openFS = hour >= 9 && hour < 21 && day > 3 && day < 6  ? true : false;
+  var openFS = hour >= 9 && hour < 21 && day > 3 && day < 6 ? true : false;
 
   if(openMT){
     $('#open-close').html('<b>Open</b>').css('color', '#4be74b');
