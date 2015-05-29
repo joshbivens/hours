6c841d007f6c09c04653433d8b95fe75c443a9cf
Josh Bivens <hotbioform@gmail.com>
2015-05-28 22:46:43 -0600

    Refactor

--- js/hours.js
+++ js/hours.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
-
+  //Date variables
   var date = new Date();
   var day = date.getDay();
   var hour = date.getHours();
 
+  //Logic variables
   var openMT = hour >= 9 && hour < 19 && day > 0 && day < 5 ? true : false;
-  var openFS = hour >= 9 && hour < 21 && day > 4 && day <= 6 ? true : false;
+  var openFS = hour >= 9 && hour < 21 && day > 3 && day < 6  ? true : false;
 
+  //Tags
+  var openBox = $('#openbox');
+  var open = $('<b>Open</b>').css('color', '#4be74b');
+  var closed = $('<b>Closed</b>').css('color', '#fb260e');
+
+  //Checks the day/hour; If open, displays "Open", else displays "Closed"
   if(openMT){
-    $('#open-close').html('<b>Open</b>').css('color', '#4be74b');
+    openBox.html(open);
   }else if(openFS){
-    $('#open-close').html('<b>Open</b>').css('color', '#4be74b');
+    openBox.html(open);
   }else{
-    $('#open-close').html('<b>Closed</b>').css('color', '#fb260e');
+    openBox.html(closed);
   }
 
 });
