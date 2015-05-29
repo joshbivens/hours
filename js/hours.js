9b181d3327b5c77a56527d5459739a6a4f91b124
Josh Bivens <hotbioform@gmail.com>
2015-05-29 10:25:30 -0600

    Add openFor/closedFor/new logic vars

--- js/hours.js
+++ js/hours.js
@@ -7,9 +7,11 @@ $(document).ready(function () {
   //Logic variables
   var openMT = hour >= 9 && hour < 19 && day > 0 && day < 5 ? true : false;
   var openFS = hour >= 9 && hour < 21 && day > 3 && day < 6  ? true : false;
+	var closedMT = hour >= 19 && hour < 9 && day > 0 && day < 5 ? true : false;
+	var closedFS = hour >= 21 && hour < 9 && day > 3 && day < 6 ? true : false;
 
   //Tags
-  var openBox = $('#openbox');
+  var openBox = $('ul:last-child li:first-child');
   var open = $('<b>Open</b>').css('color', '#4be74b');
   var closed = $('<b>Closed</b>').css('color', '#fb260e');
 
@@ -21,5 +23,24 @@ $(document).ready(function () {
   } else {
     openBox.html(closed);
   }
+
+	//Returns hours left till close
+	var openFor = function () {
+		var x = 0;
+		if (openMT) {
+			x = 19 - hour;
+		} else if (openFS) {
+			x = 21 - hour;
+		}
+		return x;
+	};
+
+	//Return hours left till open
+	var closedFor = function () {
+		var x = 0;
+		if (closedMT) {
+			x = Math.abs(hour)
+		}
+	}
 
 });
