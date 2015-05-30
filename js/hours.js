847c2be03945b2cb16f2f4e244a0249d649f3785
Josh Bivens <hotbioform@gmail.com>
2015-05-30 09:17:38 -0600

    Test

--- js/hours.js
+++ js/hours.js
@@ -6,38 +6,38 @@ $(document).ready(function () {
       hour = date.getHours();
 
 //Logic variables
-var openMT = hour > 8 && hour < 19 && day > 0 && day < 5 ? true : false,
-openFS = hour > 8 && hour < 21 && day === 5 || day === 6  ? true : false,
-closedMT = hour > 18 && day > 0 && day < 5 ? true : false,
-closedFS = hour > 20 && day === 5 || day === 6 ? true : false;
+  var openMT = hour > 8 && hour < 19 && day > 0 && day < 5 ? true : false,
+      openFS = hour > 8 && hour < 21 && day === 5 || day === 6  ? true : false,
+      closedMT = hour > 18 && day > 0 && day < 5 ? true : false,
+      closedFS = hour > 20 && day === 5 || day === 6 ? true : false;
 
 //Tags
-var openBox = $('ul:last-child li:first-child'),
-open = $('<b><u>Open</u></b>').css('color', '#4be74b'),
-closed = $('<b><u>Closed</u></b>').css('color', '#fb260e');
+  var openBox = $('ul:last-child li:first-child'),
+      open = $('<b><u>Open</u></b>').css('color', '#4be74b'),
+      closed = $('<b><u>Closed</u></b>').css('color', '#fb260e');
 
 //Checks the day/hour; If open, displays "Open", else displays "Closed"
-if (openMT || openFS) {
-openBox.html(open);
+  if (openMT || openFS) {
+    openBox.html(open);
 //Inserts a message with either openFor or closedFor
 //into the title attr of a tooltip when openBox is hovered
-$(openBox).on('mouseover', function(){
-if (openFor === 1) {
-$(this).prop('title', 'We\'re open for another hour');
-} else {
-$(this).prop('title', 'We\'re open for another ' + openFor + ' hours');
-}
-})
-} else {
-openBox.html(closed);
-$(openBox).on('mouseover', function(){
-if (closedFor === 1) {
-$(this).prop('title', 'We\'ll be closed for another hour');
-} else {
-$(this).prop('title', 'We\'ll be closed for another ' + closedFor + ' hour(s)');
-}
-})
-}
+    $(openBox).on('mouseover', function(){
+      if (openFor === 1) {
+        $(this).prop('title', 'We\'re open for another hour');
+      } else {
+        $(this).prop('title', 'We\'re open for another ' + openFor + ' hours');
+      }
+    })
+  } else {
+    openBox.html(closed);
+    $(openBox).on('mouseover', function(){
+      if (closedFor === 1) {
+        $(this).prop('title', 'We\'ll be closed for another hour');
+      } else {
+        $(this).prop('title', 'We\'ll be closed for another ' + closedFor + ' hour(s)');
+      }
+    })
+  }
 
 //Returns hours left till close
 var openFor = (function () {
