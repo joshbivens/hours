3f539062fe9e6d7a10b02991fd68402253e80301
Josh Bivens <hotbioform@gmail.com>
2015-05-31 22:38:31 -0600

    Fix openFor/closeFor

--- js/hours.js
+++ js/hours.js
@@ -1,27 +1,34 @@
-$(document).ready(function () {
-
-//Date variables
+$(document).ready(function() {
+  //Date variables
   var date = new Date(),
       day = date.getDay(),
       hour = date.getHours();
 
-//Logic variables
+  //Logic variables
   var openMT = hour > 8 && hour < 19 && day > 0 && day < 5 ? true : false,
-      openFS = hour > 8 && hour < 21 && day === 5 || day === 6  ? true : false,
+      openFS = hour > 8 && hour < 21 && day === 5 || day === 6 ? true : false,
       closedMT = hour > 18 && day > 0 && day < 5 ? true : false,
       closedFS = hour > 20 && day === 5 || day === 6 ? true : false;
 
-//Tags
+  //Sun - 0
+  //Mon - 1
+  //Tue - 2
+  //Wed - 3
+  //Thu - 4
+  //Fri - 5
+  //Sat - 6
+
+  //Tags
   var openBox = $('ul:last-child li:first-child'),
       open = $('<b><u>Open</u></b>').css('color', '#4be74b'),
       closed = $('<b><u>Closed</u></b>').css('color', '#fb260e');
 
-//Checks the day/hour; If open, displays "Open", else displays "Closed"
+  //Checks the day/hour; If open, displays "Open", else displays "Closed"
   if (openMT || openFS) {
     openBox.html(open);
-//Inserts a message with either openFor or closedFor
-//into the title attr of a tooltip when openBox is hovered
-    $(openBox).on('mouseover', function(){
+    //Inserts a message with either openFor or closedFor
+    //into the title attr of a tooltip when openBox is hovered
+    $(openBox).on('mouseover', function() {
       if (openFor === 1) {
         $(this).prop('title', 'We\'re open for another hour');
       } else {
@@ -30,17 +37,16 @@ $(document).ready(function () {
     })
   } else {
     openBox.html(closed);
-    $(openBox).on('mouseover', function(){
+    $(openBox).on('mouseover', function() {
       if (closedFor === 1) {
         $(this).prop('title', 'We\'ll be closed for another hour');
       } else {
-        $(this).prop('title', 'We\'ll be closed for another ' + closedFor + ' hour(s)');
+        $(this).prop('title', 'We\'ll be closed for another ' + closedFor + ' hours');
       }
     })
   }
-
-//Returns hours left till close
-  var openFor = (function () {
+  //Returns hours left till close
+  var openFor = (function() {
     var x = 0;
     if (openMT) {
       x = 18 - hour;
@@ -50,13 +56,23 @@ $(document).ready(function () {
     return x;
   })();
 
-//Return hours left till open
-  var closedFor = (function () {
+  //Return hours left till open
+  var closedFor = (function() {
     var x = 0;
     if (closedMT || closedFS) {
-      return hour === 0 ? x = 9 : x = 33 - hour;
+      if(hour === 0){
+        x = 9;
+      }else if(hour > 0){
+        x = 9 - hour;
+      }else if(hour > 17){
+        x = 33 - hour;
+      }
     } else { //if Sunday
-      return hour === 0 ? x = 33 : x = 57 - hour;
+      if(hour === 0){
+        x = 33;
+      }else if(hour > 0 && hour < 24){
+        x = 33 - hour;
+      }
     }
     return x;
   })();
