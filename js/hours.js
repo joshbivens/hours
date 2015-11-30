e57b34e9b3cdc9389783b1e03a6a2e47efbf6ad6
Josh Bivens <hotbioform@gmail.com>
2015-11-30 08:24:10 -0700

    Remove comments

--- js/hours.js
+++ js/hours.js
@@ -1,34 +1,23 @@
 $(document).ready(function() {
-  //Date variables
+
   var date = new Date(),
       day = date.getDay(),
       hour = date.getHours(),
       minutes = date.getMinutes(),
       minFrom60 = 60 - minutes;
 
-  //Logic variables
   var openMT = (8 < hour && hour < 18) && (0 < day && day < 5) ? true : false,
       openFS = (8 < hour && hour < 20) && (day === 5 || day === 6) ? true : false,
       closedMT = (hour > 17 || hour < 9) && (0 < day && day < 5) ? true : false,
       closedFS = (hour > 19 || hour < 9) && (day === 5 || day === 6) ? true : false;
-  //Sun - 0
-  //Mon - 1
-  //Tue - 2
-  //Wed - 3
-  //Thu - 4
-  //Fri - 5
-  //Sat - 6
 
-  //Tags
   var openBox = $('ul:last-child li:first-child'),
       open = $('<b>Open</b><i class="fa fa-question-circle"></i>').css('color', '#4be74b'),
       closed = $('<b>Closed</b><i class="fa fa-question-circle"></i>').css('color', '#fb260e');
 
-  //Checks the day/hour; If open, displays "Open", else displays "Closed"
   if (openMT || openFS) {
     openBox.html(open);
-    //Inserts a message with either openFor or closedFor
-    //into the title attr of a tooltip when openBox is hovered
+
     $(openBox).on('mouseover', function() {
       if (openFor === 0) {
         $(this).prop('title', 'We\'re open for ' + minFrom60 + ' more minutes');
@@ -50,7 +39,7 @@ $(document).ready(function() {
       }
     })
   }
-  //Returns hours left till close
+
   var openFor = (function() {
     var x = 0;
     if (openMT) {
@@ -61,7 +50,6 @@ $(document).ready(function() {
     return x;
   })();
 
-  //Return hours left till open
   var closedFor = (function() {
     var x = 0;
     if (closedMT || closedFS) {
@@ -72,7 +60,7 @@ $(document).ready(function() {
       }else if(hour > 17){
         x = 33 - hour;
       }
-    } else { //if Sunday
+    } else {
       if(hour === 0){
         x = 33;
       }else if(hour > 0 && hour < 24){
